aa836bf11b240fa643445f65555d24359269d1db
Dongi <chaedonggi@gmail.com>
2022-04-20 07:58:39 +0900

    chat complete

--- src/server.js
+++ src/server.js
@@ -19,13 +19,15 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
   socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", message => {
-    console.log(message.toString('utf8'));
-    });
-  socket.send("hello!!!");
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
 });
 
 server.listen(3000, handleListen);
